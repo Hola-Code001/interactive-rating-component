64e5ac4c5e77b1a21b505e220d4633d9fe2822a1
Hola-Code001 <112684212+Hola-Code001@users.noreply.github.com>
2023-03-26 00:37:29 +0100

    Update main.js

--- main.js
+++ main.js
@@ -5,7 +5,7 @@ let selected = document.querySelector('#rating-star')
 var ratingBtn = document.querySelectorAll('.rating-btn').length;
 
 for(var i = 0; i < ratingBtn; i++){
-  document.querySelectorAll('.rating-btn')[i].addEventListener('click', function(e){
+  document.querySelectorAll('.rating-btn')[i].addEventListener('click', function(){
     var buttonHtml = this.innerHTML;
     selected.innerHTML = buttonHtml;
     
